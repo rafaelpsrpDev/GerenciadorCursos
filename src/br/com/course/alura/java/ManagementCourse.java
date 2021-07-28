94bc6d3f65210913183b8037c739a84d6a72e876
rafaelpsrpDev <63820802+rafaelpsrpDev@users.noreply.github.com>
2021-07-27 21:21:09 -0300

    Add files via upload

---
+++ src/br/com/course/alura/java/ManagementCourse.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.course.alura.java;
+
+/**
+ * @author Rafael Paulo
+ *
+ */
+public class ManagementCourse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
