149c82b0021787a210c7fdf37cc57bf43568f82f
rafaelpsrpDev <rafaelpsrp@outlook.com>
2021-07-27 22:55:35 -0300

    ArrayList -> add, remove, size(), isEmpty(), get, set list.forEach(l) Collections.sort

--- src/br/com/course/alura/java/ManagementCourse.java
+++ src/br/com/course/alura/java/ManagementCourse.java
@@ -3,6 +3,10 @@
  */
 package br.com.course.alura.java;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * @author Rafael Paulo
  *
@@ -13,7 +17,21 @@ public class ManagementCourse {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ArrayList<String> aulas = new ArrayList<String>();
+		aulas.add("Matemática");
+		aulas.add("Biologia");
+		aulas.add("História");
+		aulas.add("Português");
+		aulas.add("Inglês");
+		Collections.sort(aulas);
+		if (aulas.isEmpty()) {
+			System.out.println("Está vazio");
+		} else {
+			aulas.forEach(aula -> {
+				System.out.println(aula);
+			});
+		}
 
 	}
 
